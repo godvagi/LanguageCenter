ce35db8351f7525707bbecd7cf810230641f1d92
poramain keawpan <poramain.k@ku.th>
2017-05-08 13:45:20 +0700

    add logo images

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>About Us | Corlate</title>
+    <title>About Us</title>
 
 	<!-- core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -19,11 +19,7 @@
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+    
 </head><!--/head-->
 
 <body>
@@ -33,8 +29,7 @@
     <section id="about-us">
         <div class="container">
 			<div class="center wow fadeInDown">
-				<h2>About Corlate</h2>
-				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
+				<h2>เกี่ยวกับเรา Easy Language</h2>
 			</div>
 
 			<!-- about us slider -->
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,36 @@
 @extends('layouts.master')
+@section('content')
+
+<p>hello world</p>
+<div id="about-slider">
+				<div id="carousel-slider" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+				  	<ol class="carousel-indicators visible-xs">
+					    <li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-slider" data-slide-to="1"></li>
+					    <li data-target="#carousel-slider" data-slide-to="2"></li>
+				  	</ol>
+
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="images/slider/bg1.jpg" class="img-responsive" alt="">
+					   </div>
+					   <div class="item">
+							<img src="images/slider/bg2.jpg" class="img-responsive" alt="">
+					   </div>
+					   <div class="item">
+							<img src="images/slider/bg3.jpg" class="img-responsive" alt="">
+					   </div>
+					</div>
+
+					<a class="left carousel-control hidden-xs" href="#carousel-slider" data-slide="prev">
+						<i class="fa fa-angle-left"></i>
+					</a>
+
+					<a class=" right carousel-control hidden-xs"href="#carousel-slider" data-slide="next">
+						<i class="fa fa-angle-right"></i>
+					</a>
+				</div> <!--/#carousel-slider-->
+			</div><!--/#about-slider-->
+
+@endsection
